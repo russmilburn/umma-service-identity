ae82100d592c27c4ff75c2e6edb1c15aa308a00c
Russell Milburn <russell.milburn@team.telstra.com>
2018-05-13 22:56:30 -0600

    updated shell script

--- pipeline/scripts/run-tests.sh
+++ pipeline/scripts/run-tests.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 echo '[SCRIPT] installing dev dependencies'
-
+set -x
 npm install --save-dev cross-env
-
+set +x
 echo '[SCRIPT] running test'
 
+set -x
 npm test
 
 echo '[SCRIPT] complete'
