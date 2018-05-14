6b0a83e7d6e9a64c6e7d8a3f15a6e2edd6be9a30
Russell Milburn <russell.milburn@team.telstra.com>
2018-05-14 14:04:49 -0600

    Updated test cases

--- tests/LoginUnitTest.js
+++ tests/LoginUnitTest.js
@@ -1,17 +1,25 @@
 const assert = require('assert');
 
 describe('Login Test Suite', function () {
-  
-  
-  describe('Test cases for Login', function () {
-    
-    
-    it('[TC1: Success] POST /login {username:String, password: String}', function () {
-      const LoginModule = require('./../src/modules/LoginModule');
-      
-      let module = new LoginModule();
-      assert.equal(module.login('russell','1234'), true);
+
+
+    describe('Test cases for Login', function () {
+
+
+        it('[TC1: Success] POST /login {username:String, password: String}', function () {
+            const LoginModule = require('./../src/modules/LoginModule');
+
+            let module = new LoginModule();
+            assert.equal(module.login('russell', '1234'), true);
+        })
+
+        it('[TC2: Success] POST /login {username:String, password: String}', function () {
+            const LoginModule = require('./../src/modules/LoginModule');
+
+            let module = new LoginModule();
+            assert.equal(module.login('russell', '1234'), true);
+        })
+
+
     })
-    
-  })
 });
